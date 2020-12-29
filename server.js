7753bf81f27103f8abebd9d3d9bdc4574f67e225
Khali <joekingsleymukundi@gmail.com>
2020-12-29 16:44:10 +0300

    house db integrating

--- server.js
+++ server.js
@@ -52,12 +52,43 @@ const userSchema = new mongoose.Schema({
   profileimg: String,
   googleId: String,
   facebookId: String,
+  houseId: [String],
+});
+const houseSchema = new mongoose.Schema({
+  ownerId: String,
+  ownername: String,
+  ownerphone: String,
+  ownerdp: String,
+  renterId: String,
+  title: String,
+  status: String,
+  city: String,
+  price: String,
+  paymentStatus: String,
+  reviews: [
+    {
+      name: String,
+      comments: String,
+      rating: String,
+    },
+  ],
+  rooms: String,
+  births: String,
+  area: String,
+  areacode: String,
+  address: String,
+  type: String,
+  renters: String,
+  housedp: String,
+  housepics: [String],
+  speciality: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
+const House = new mongoose.model("House", houseSchema);
 
 passport.use(User.createStrategy());
 
@@ -108,8 +139,6 @@ passport.use(
   )
 );
 
-const port = process.env.PORT || 8000;
-
 app.use("/upload", express.static("upload"));
 
 app.get("/", (req, res) => {
@@ -147,12 +176,45 @@ app.get("/home", (req, res) => {
         console.log(err);
       } else {
         if (foundusers) {
-          console.log(foundusers);
-          const username = foundusers.username;
-          const profileDp = foundusers.profileimg;
-          res.render("home.ejs", {
-            userdisplayname: username,
-            profileimage: profileDp,
+          House.find({}, (err, foundhouses) => {
+            if (foundhouses.length != 0) {
+              console.log(foundhouses);
+              const username = foundusers.username;
+              const profileDp = foundusers.profileimg;
+              const role = foundusers.role;
+              res.render("home.ejs", {
+                Housedbstatus: foundhouses.length,
+                userdisplayname: username,
+                profileimage: profileDp,
+                userRole: role,
+                houses: foundhouses,
+                // houseImage: foundhouse.housedp,
+                // prices: foundhouse.price,
+                // listerimage: foundowner.profileimg,
+                // housetitle: foundhouse.title,
+                // address: foundhouse.price,
+                // area: foundhouse.area,
+                // city: foundhouse.area,
+                // housenumber: foundhouse.address,
+                // rooms: foundhouse.rooms,
+                // births: foundhouse.births,
+                // renters: foundhouse.renters,
+                // type: foundhouse.type,
+                // listername: foundowner.fullname,
+              });
+            } else if (foundhouses.length == 0) {
+              console.log("no houses");
+              console.log("user" + req.user);
+              const username = foundusers.username;
+              const profileDp = foundusers.profileimg;
+              const role = foundusers.role;
+              res.render("home.ejs", {
+                Housedbstatus: foundhouses.length,
+                userdisplayname: username,
+                profileimage: profileDp,
+                userRole: role,
+              });
+            }
           });
         }
       }
@@ -203,41 +265,319 @@ app.get(
 );
 
 app.get("/listing", (req, res) => {
-  res.render("listing.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("listing.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/Appartment", (req, res) => {
-  res.render("Appartment.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("Appartment.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/bed-breakfast", (req, res) => {
-  res.render("bed-breakfast.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("bed-breakfast.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/condo", (req, res) => {
-  res.render("condo.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("condo.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/house", (req, res) => {
-  res.render("house.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("house.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/studio", (req, res) => {
-  res.render("studio.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("studio.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/loft", (req, res) => {
-  res.render("loft.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("studio.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/blog", (req, res) => {
-  res.render("blog.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("blog.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 app.get("/host-registration", (req, res) => {
-  res.render("hostreg.ejs");
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("hostreg.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/host-registration", (req, res) => {
+  User.updateOne({ _id: req.user.id }, { role: "homey_host" }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("success");
+      res.redirect("/home");
+    }
+  });
+});
+
+app.get("/lisingproperty", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.findById({ _id: req.user.id }, (err, foundusers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundusers) {
+          console.log(foundusers);
+          const username = foundusers.username;
+          const profileDp = foundusers.profileimg;
+          const role = foundusers.role;
+          res.render("propertylisting.ejs", {
+            userdisplayname: username,
+            profileimage: profileDp,
+            userRole: role,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/lisingproperty", upload.single("houseDp"), (req, res) => {
+  const houselisting = new House({
+    ownerId: req.user.id,
+    ownername: req.user.fullname,
+    ownerphone: req.user.phone,
+    ownerdp: req.user.profileimg,
+    title: req.body.title,
+    price: req.body.price,
+    rooms: req.body.rooms,
+    city: req.body.city,
+    area: req.body.area,
+    areacode: req.body.code,
+    births: req.body.births,
+    address: req.body.address,
+    type: req.body.houseType,
+    renters: req.body.population,
+    housedp: req.file.path,
+  });
+  houselisting.save((err) => {
+    if (err) {
+      console.log("error in  saving is" + err);
+      res.redirect("/lisingproperty");
+    } else {
+      House.find({ ownerId: req.user.id }, (err, foundhouses) => {
+        if (err) {
+          console.log("error in finding owner id is " + err);
+        } else {
+          User.findById({ _id: req.user.id }, (err, founduser) => {
+            if (err) {
+              console.log(
+                "the err in looking through the user to find the lister od a plot so ass to ensure no repetition in the house id arry is " +
+                  err
+              );
+            } else {
+              const housearry = founduser.houseId;
+              console.log(foundhouses._id);
+              console.log(foundhouses);
+              foundhouses.forEach((foundhouse) => {
+                if (housearry.includes(foundhouse._id)) {
+                  console.log("error house already exists");
+                  console.log(foundhouses._id);
+                } else {
+                  User.updateOne(
+                    { _id: req.user.id },
+                    { $push: { houseId: foundhouse._id } },
+                    (err) => {
+                      if (err) {
+                        console.log("the error in updating house id is " + err);
+                      } else {
+                        console.log("success");
+                        res.redirect("/home");
+                      }
+                    }
+                  );
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 app.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log("server is live at port " + port);
 });
-
-clientid =
-  "39775136496-0ekont6rhvnn7isv10i21ofsbf7bus4v.apps.googleusercontent.com";
-clientsecret = "MijPVGfzusEWzNkit1zSdrNz";
